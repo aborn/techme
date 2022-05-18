59c68033736e1b1ea3245c4fcb5618fe09bc7dcd
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-18 16:34:31 +0800

    nuxt content official

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,33 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        head: {
+          link: [
+            {
+              rel: 'stylesheet',
+              href: 'https://unpkg.com/@picocss/pico@latest/css/pico.min.css'
+            }
+          ]
+        }
+    },
     modules: [
         '@nuxt/content',
         '@nuxt/ui',
         '@vueuse/nuxt',
-        '@nuxtjs/color-mode'
+        '@nuxtjs/color-mode',
+        '@nuxthq/admin'
     ],
+    rootDir: __dirname,
+    content: {
+        navigation: {
+          fields: ['icon']
+        },
+        highlight: {
+          theme: 'one-dark-pro',
+          preload: ['json', 'js', 'ts', 'html', 'css', 'vue']
+        }
+    },
     typescript: {
         shim: false
     }
